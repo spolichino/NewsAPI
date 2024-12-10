6fb1fd0cc9da27be68ed44ff638a558df4d920a0
spolichino <sam.polichino@gmail.com>
2024-12-10 14:44:26 -0500

    this is very sad.

--- NewsAPI/ContentView.swift
+++ NewsAPI/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Goodbye, world!")
+            Text("Goodbye cruel, world!")
         }
         .padding()
     }
